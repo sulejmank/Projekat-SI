5fe64e91723a8b0873aa92ad33e768cd43a48e19
Sulejman Karisik <6103201@gmail.com>
2017-07-31 13:36:11 +0200

    Add files via upload

---
+++ public/js/viewer.js
@@ -0,0 +1,42 @@
+let map;
+let markers = new Map();
+
+document.addEventListener('DOMContentLoaded', () => {
+  const socket = io('/');
+
+  socket.on('locationsUpdate', locations => {
+    markers.forEach((marker, id) => {
+      marker.setMap(null);
+      markers.delete(id);
+    });
+
+    locations.forEach(([id, position]) => {
+      if (position.lat && position.lng) {
+        const marker = new google.maps.Marker({
+          position,
+          map,
+          label:{
+            text:'2'
+          }
+        });
+        markers.set(id, marker);
+      }
+    });
+  });
+
+  setInterval(() => {
+    socket.emit('requestLocations')
+  }, 2000)
+});
+
+function initMap() {
+  navigator.geolocation.getCurrentPosition(pos => {
+    const { latitude: lat, longitude: lng } = pos.coords
+    map = new google.maps.Map(document.getElementById('map'), {
+      center: { lat, lng },
+      zoom: 12
+    });
+  }, err => {
+    console.error(err);
+  });
+}
